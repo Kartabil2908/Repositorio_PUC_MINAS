8087631e8932065e2f7b162ea7ad9454fd5a3eb9
Bernardo Ladeira Kartabil <beladeira1.000@gmail.com>
2024-10-07 14:10:11 -0300

    tpQ01

---
+++ Terceiro-Semestre/Algoritmo-e-estruturas-de-dados-II-AEDSII/tps_resolvidos/tp02/tp02.c
@@ -0,0 +1,21 @@
+
+/*#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <
+
+
+typedef struct Pokemon
+{
+    int id;
+    int generation;
+    char[50] name;
+    char[50] description;
+    types;
+    abilities;
+    double weight;
+    double height;
+    int captureRate;
+    isLegendary;
+    Date captureDate;
+}*/
